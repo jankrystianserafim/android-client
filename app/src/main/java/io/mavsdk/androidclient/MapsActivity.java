678c26188e22d6b6db21ed1daed654289e2ebf26
ddalkyTokky <tnsdyd6933@gmail.com>
2022-07-06 03:33:35 +0900

    first

---
+++ app/src/main/java/io/mavsdk/androidclient/MapsActivity.java
@@ -0,0 +1,259 @@
+package io.mavsdk.androidclient;
+
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.plugins.annotation.Circle;
+import com.mapbox.mapboxsdk.plugins.annotation.CircleManager;
+import com.mapbox.mapboxsdk.plugins.annotation.CircleOptions;
+import com.mapbox.mapboxsdk.plugins.annotation.Symbol;
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager;
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions;
+import com.mapbox.mapboxsdk.utils.ColorUtils;
+import io.mavsdk.System;
+import io.mavsdk.mavsdkserver.MavsdkServer;
+import io.mavsdk.repositories.DroneRepository;
+import io.reactivex.disposables.Disposable;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main Activity to display map and create missions.
+ * 1. Take off
+ * 2. Long click on map to add a waypoint
+ * 3. Hit play to start mission.
+ */
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+  private static final Logger logger = LoggerFactory.getLogger(MapsActivity.class);
+
+  public static final String BACKEND_IP_ADDRESS = "127.0.0.1";
+
+  private CircleManager circleManager;
+  private SymbolManager symbolManager;
+
+  private MapView mapView;
+  private MapboxMap map;
+
+  private MapsViewModel viewModel;
+  private Symbol currentPositionMarker;
+
+  private MavsdkServer mavsdkServer = new MavsdkServer();
+  private System drone;
+  private DroneRepository mDroneRepository;
+  private final List<Circle> waypoints = new ArrayList<>();
+  private final List<Disposable> disposables = new ArrayList<>();
+
+  private Observer<LatLng> currentPositionObserver = this::updateVehiclePosition;
+  private Observer<List<LatLng>> currentMissionPlanObserver = this::updateMarkers;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Mapbox.getInstance(this, getString(R.string.access_token));
+    setContentView(R.layout.activity_maps);
+    mapView = findViewById(R.id.mapView);
+    mapView.onCreate(savedInstanceState);
+    mapView.getMapAsync(this);
+
+    viewModel = ViewModelProviders.of(this).get(MapsViewModel.class);
+
+    FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+    floatingActionButton.setOnClickListener(v -> viewModel.startMission(drone));
+
+    mDroneRepository = DroneRepository.getInstance(getApplication());
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    mapView.onStart();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    mapView.onResume();
+    viewModel.currentPositionLiveData.observe(this, currentPositionObserver);
+    viewModel.currentMissionPlanLiveData.observe(this, currentMissionPlanObserver);
+
+    //int mavsdkServerPort = mavsdkServer.run();
+    //drone = new System(BACKEND_IP_ADDRESS, mavsdkServerPort);
+
+    mavsdkServer = mDroneRepository.getmMavsdkServer();
+    drone = mDroneRepository.getmDrone();
+
+    disposables.add(drone.getTelemetry().getFlightMode().distinctUntilChanged()
+        .subscribe(flightMode -> logger.debug("flight mode: " + flightMode)));
+    disposables.add(drone.getTelemetry().getArmed().distinctUntilChanged()
+        .subscribe(armed -> logger.debug("armed: " + armed)));
+    disposables.add(drone.getTelemetry().getPosition().subscribe(position -> {
+      LatLng latLng = new LatLng(position.getLatitudeDeg(), position.getLongitudeDeg());
+      viewModel.currentPositionLiveData.postValue(latLng);
+    }));
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    mapView.onPause();
+    viewModel.currentPositionLiveData.removeObserver(currentPositionObserver);
+    viewModel.currentMissionPlanLiveData.removeObserver(currentMissionPlanObserver);
+
+    for (Disposable disposable : disposables) {
+      disposable.dispose();
+    }
+    disposables.clear();
+
+
+    drone.dispose();
+    drone = null;
+    mavsdkServer.stop();
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    mapView.onStop();
+  }
+
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    mapView.onLowMemory();
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    mapView.onDestroy();
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    mapView.onSaveInstanceState(outState);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_maps, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle item selection
+    switch (item.getItemId()) {
+      case R.id.disarm:
+        drone.getAction().kill().subscribe();
+        break;
+      case R.id.land:
+        drone.getAction().land().subscribe();
+        break;
+      case R.id.return_home:
+        drone.getAction().returnToLaunch().subscribe();
+        break;
+      case R.id.takeoff:
+        drone.getAction().arm().andThen(drone.getAction().takeoff()).subscribe();
+        break;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+    return true;
+  }
+
+  /**
+   * Update [currentPositionMarker] position with a new [position].
+   *
+   * @param newLatLng new position of the vehicle
+   */
+  private void updateVehiclePosition(@Nullable LatLng newLatLng) {
+    if (newLatLng == null || map == null || symbolManager == null) {
+      // Not ready
+      return;
+    }
+
+    // Add a vehicle marker and move the camera
+    if (currentPositionMarker == null) {
+      SymbolOptions symbolOptions = new SymbolOptions();
+      symbolOptions.withLatLng(newLatLng);
+      symbolOptions.withIconImage("marker-icon-id");
+      currentPositionMarker = symbolManager.create(symbolOptions);
+
+      map.moveCamera(CameraUpdateFactory.tiltTo(0));
+      map.moveCamera(CameraUpdateFactory.newLatLngZoom(newLatLng, 14.0f));
+    } else {
+      currentPositionMarker.setLatLng(newLatLng);
+      symbolManager.update(currentPositionMarker);
+    }
+  }
+
+  /**
+   * Update the [map] with the current mission plan waypoints.
+   *
+   * @param latLngs current mission waypoints
+   */
+  private void updateMarkers(@NonNull List<LatLng> latLngs) {
+    if (circleManager != null) {
+      circleManager.delete(waypoints);
+      waypoints.clear();
+    }
+
+    for (LatLng latLng : latLngs) {
+      CircleOptions circleOptions = new CircleOptions()
+          .withLatLng(latLng)
+          .withCircleColor(ColorUtils.colorToRgbaString(Color.BLUE))
+          .withCircleStrokeColor(ColorUtils.colorToRgbaString(Color.BLACK))
+          .withCircleStrokeWidth(1.0f)
+          .withCircleRadius(12f)
+          .withDraggable(false);
+
+      circleManager.create(circleOptions);
+    }
+  }
+
+  /**
+   * Manipulates the map once available.
+   * This callback is triggered when the map is ready to be used.
+   * This is where we can add markers or lines, add listeners or move the camera.
+   */
+  @Override
+  public void onMapReady(@NonNull MapboxMap mapboxMap) {
+    mapboxMap.getUiSettings().setRotateGesturesEnabled(false);
+    mapboxMap.getUiSettings().setTiltGesturesEnabled(false);
+    mapboxMap.addOnMapLongClickListener(point -> {
+      viewModel.addWaypoint(point);
+      return true;
+    });
+
+    mapboxMap.setStyle(Style.LIGHT, style -> {
+      // Add the marker image to map
+      style.addImage("marker-icon-id",
+          BitmapFactory.decodeResource(
+              MapsActivity.this.getResources(), R.drawable.mapbox_marker_icon_default));
+
+      symbolManager = new SymbolManager(this.mapView, this.map, style);
+      symbolManager.setIconAllowOverlap(true);
+      circleManager = new CircleManager(this.mapView, this.map, style);
+    });
+
+    map = mapboxMap;
+  }
+}
---
+++ app/src/main/java/io/mavsdk/repositories/DroneRepository.java
@@ -0,0 +1,313 @@
+package io.mavsdk.repositories;
+
+import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+
+import io.mavsdk.androidclient.BuildConfig;
+import io.mavsdk.androidclient.R;
+import com.gen.mavsdkandroidserialtest.io.TcpInputOutputManager;
+import com.gen.mavsdkandroidserialtest.models.PositionRelative;
+import com.gen.mavsdkandroidserialtest.models.Speed;
+import com.google.common.collect.Lists;
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import io.mavsdk.System;
+import io.mavsdk.mavsdkserver.MavsdkServer;
+import io.mavsdk.telemetry.Telemetry;
+import io.reactivex.Flowable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class DroneRepository {
+    private static final String TAG = "LOG_" + DroneRepository.class.getSimpleName();
+    private static final boolean GENERATE_DUMMY_DATA = false;
+    private static final boolean IS_SITL = false;
+
+    private static final String MAVSDK_SERVER_IP = "127.0.0.1";
+    private static final int TCP_SERVER_PORT = 8888;
+    private static final int USB_BAUD_RATE = 57600;
+    private static final int BUFFER_SIZE = 2048;
+    private static final int IO_TIMEOUT = 1000;
+    private static final long THROTTLE_TIME_MILLIS = 500;
+
+    private static DroneRepository instance;
+
+    private final Context mAppContext;
+    private final CompositeDisposable mCompositeDisposable;
+    private System mDrone;
+    private MavsdkServer mMavsdkServer;
+    private SerialInputOutputManager mSerialManager;
+    private TcpInputOutputManager mTcpManager;
+
+    private LiveData<PositionRelative> mPositionRelativeLiveData;
+    private LiveData<Speed> mSpeedLiveData;
+    private LiveData<Telemetry.Battery> mBatteryLiveData;
+    private LiveData<Telemetry.GpsInfo> mGpsInfoLiveData;
+    private LiveData<Telemetry.Position> mPositionLiveData;
+
+    private static final String INTENT_ACTION_GRANT_USB = BuildConfig.APPLICATION_ID + ".GRANT_USB";
+
+    public static DroneRepository getInstance(@NonNull Application application) {
+        if (instance == null) {
+            instance = new DroneRepository(application);
+        }
+        return instance;
+    }
+
+    private DroneRepository(Application application) {
+        mAppContext = application.getApplicationContext();
+        mCompositeDisposable = new CompositeDisposable();
+
+        if (IS_SITL) {
+            initializeServerAndDrone("udp://:14540"); // Default port
+        } else {
+            initializeUsbAndTcp();
+            initializeServerAndDrone("tcp://:" + TCP_SERVER_PORT);
+        }
+    }
+
+    private void initializeUsbAndTcp() {
+        UsbManager usbManager = (UsbManager) mAppContext.getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> driverList = UsbSerialProber.getDefaultProber().findAllDrivers(usbManager);
+        if (driverList.isEmpty()) {
+            Toast.makeText(mAppContext, R.string.str_usb_device_not_found, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        UsbSerialDriver usbSerialDriver = driverList.get(0);
+        UsbDevice usbDevice = usbSerialDriver.getDevice();
+        boolean hasPermission = usbManager.hasPermission(usbDevice);
+
+        if (!hasPermission) {
+            Toast.makeText(mAppContext, R.string.str_usb_permission_not_granted, Toast.LENGTH_SHORT).show();
+            PendingIntent usbPermissionIntent = PendingIntent.getBroadcast(mAppContext, 0, new Intent(INTENT_ACTION_GRANT_USB), 0);
+            usbManager.requestPermission(usbSerialDriver.getDevice(), usbPermissionIntent);
+            hasPermission = usbManager.hasPermission(usbDevice);
+            if (!hasPermission) {
+                return;
+            }
+        }
+
+        Log.d(TAG, "initializeUsbDevice: hasPermission: " + usbManager.hasPermission(usbDevice));
+
+        UsbSerialPort usbSerialPort = usbSerialDriver.getPorts().get(0);
+        UsbDeviceConnection connection = usbManager.openDevice(usbDevice);
+
+        try {
+            usbSerialPort.open(connection);
+            usbSerialPort.setParameters(
+                    USB_BAUD_RATE,
+                    UsbSerialPort.DATABITS_8,
+                    UsbSerialPort.STOPBITS_1,
+                    UsbSerialPort.PARITY_NONE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mSerialManager = new SerialInputOutputManager(usbSerialPort);
+        mSerialManager.setReadTimeout(IO_TIMEOUT);
+        mSerialManager.setReadBufferSize(BUFFER_SIZE);
+        mSerialManager.setWriteTimeout(IO_TIMEOUT);
+        mSerialManager.setWriteBufferSize(BUFFER_SIZE);
+
+        mTcpManager = new TcpInputOutputManager(TCP_SERVER_PORT);
+        mTcpManager.setReadBufferSize(BUFFER_SIZE);
+        mTcpManager.setWriteBufferSize(BUFFER_SIZE);
+
+        mSerialManager.setListener(new SerialInputOutputManager.Listener() {
+            @Override
+            public void onNewData(byte[] data) {
+                // Sending Serial data to TCP
+                mTcpManager.writeAsync(data);
+            }
+            @Override
+            public void onRunError(Exception e) {
+            }
+        });
+
+        mTcpManager.setListener(new TcpInputOutputManager.Listener() {
+            @Override
+            public void onNewData(byte[] data) {
+                // Sending TCP data to Serial
+                mSerialManager.writeAsync(data);
+            }
+            @Override
+            public void onRunError(Exception e) {
+            }
+        });
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(mSerialManager);
+        executorService.submit(mTcpManager);
+    }
+
+    private void initializeServerAndDrone(@NonNull String systemAddress) {
+        mMavsdkServer = new MavsdkServer();
+        int mavsdkServerPort = mMavsdkServer.run(systemAddress);
+
+        mDrone = new System(MAVSDK_SERVER_IP, mavsdkServerPort);
+    }
+
+    public MavsdkServer getmMavsdkServer() {
+        return mMavsdkServer;
+    }
+
+    public System getmDrone() {
+        return mDrone;
+    }
+
+    public LiveData<PositionRelative> getPositionRelative() {
+        if (mPositionRelativeLiveData == null) {
+            Flowable<PositionRelative> positionRelativeFlowable;
+
+            if (GENERATE_DUMMY_DATA) {
+                Random random = new Random();
+                positionRelativeFlowable = Flowable.interval(THROTTLE_TIME_MILLIS, TimeUnit.MILLISECONDS)
+                        .map(aLong -> new PositionRelative(100 * random.nextFloat(), 100 * random.nextFloat()));
+
+            } else {
+                positionRelativeFlowable = mDrone.getTelemetry().getPositionVelocityNed()
+                        .throttleLast(THROTTLE_TIME_MILLIS, TimeUnit.MILLISECONDS)
+                        .map(positionVelocityNed -> {
+                            float distance = (float) Math.hypot(positionVelocityNed.getPosition().getNorthM(), positionVelocityNed.getPosition().getEastM());
+                            float height = Math.abs(positionVelocityNed.getPosition().getDownM());
+                            return new PositionRelative(distance, height);
+                        })
+                        .subscribeOn(Schedulers.io());
+            }
+
+            mPositionRelativeLiveData = LiveDataReactiveStreams.fromPublisher(positionRelativeFlowable);
+        }
+
+        return mPositionRelativeLiveData;
+    }
+
+    public LiveData<Speed> getSpeed() {
+        if (mSpeedLiveData == null) {
+            Flowable<Speed> speedFlowable;
+
+            if (GENERATE_DUMMY_DATA) {
+                Random random = new Random();
+                speedFlowable = Flowable.interval(THROTTLE_TIME_MILLIS, TimeUnit.MILLISECONDS)
+                        .map(aLong -> new Speed(10 * random.nextFloat(), 10 * random.nextFloat()));
+            } else {
+                speedFlowable = mDrone.getTelemetry().getPositionVelocityNed()
+                        .throttleLast(THROTTLE_TIME_MILLIS, TimeUnit.MILLISECONDS)
+                        .map(positionVelocityNed -> {
+                            float hspeed = (float) Math.hypot(positionVelocityNed.getVelocity().getNorthMS(), positionVelocityNed.getVelocity().getEastMS());
+                            float vspeed = Math.abs(positionVelocityNed.getVelocity().getDownMS());
+                            return new Speed(hspeed, vspeed);
+                        })
+                        .subscribeOn(Schedulers.io());
+            }
+
+            mSpeedLiveData = LiveDataReactiveStreams.fromPublisher(speedFlowable);
+        }
+
+        return mSpeedLiveData;
+    }
+
+    public LiveData<Telemetry.Battery> getBattery() {
+        if (mBatteryLiveData == null) {
+            Flowable<Telemetry.Battery> batteryFlowable;
+
+            if (GENERATE_DUMMY_DATA) {
+                batteryFlowable = Flowable.intervalRange(0, 21, 0, THROTTLE_TIME_MILLIS, TimeUnit.MILLISECONDS)
+                        .repeat()
+                        .map(aLong -> new Telemetry.Battery((float) (16.8 + aLong * (12.6 - 16.8) / 21), 1 - aLong.floatValue() * 5 * 0.01f));
+            } else {
+                batteryFlowable = mDrone.getTelemetry().getBattery()
+                        .throttleLast(THROTTLE_TIME_MILLIS, TimeUnit.MILLISECONDS)
+                        .subscribeOn(Schedulers.io());
+            }
+
+            mBatteryLiveData = LiveDataReactiveStreams.fromPublisher(batteryFlowable);
+        }
+
+        return mBatteryLiveData;
+    }
+
+    public LiveData<Telemetry.GpsInfo> getGpsInfo() {
+        if (mGpsInfoLiveData == null) {
+            Flowable<Telemetry.GpsInfo> gpsInfoFlowable;
+
+            if (GENERATE_DUMMY_DATA) {
+                gpsInfoFlowable = Flowable.intervalRange(0, 11, 0, THROTTLE_TIME_MILLIS, TimeUnit.MILLISECONDS)
+                        .repeat()
+                        .map(aLong -> new Telemetry.GpsInfo((int) (2 * aLong), Telemetry.FixType.FIX_3D));
+            } else {
+                gpsInfoFlowable = mDrone.getTelemetry().getGpsInfo()
+                        .throttleLast(THROTTLE_TIME_MILLIS, TimeUnit.MILLISECONDS)
+                        .subscribeOn(Schedulers.io());
+            }
+
+            mGpsInfoLiveData = LiveDataReactiveStreams.fromPublisher(gpsInfoFlowable);
+        }
+        return mGpsInfoLiveData;
+    }
+
+    public LiveData<Telemetry.Position> getPosition() {
+        if (mPositionLiveData == null) {
+            Flowable<Telemetry.Position> positionFlowable;
+
+            if (GENERATE_DUMMY_DATA) {
+                List<Integer> arr = IntStream.rangeClosed(0, 50).boxed().collect(Collectors.toList());
+                arr.addAll(Lists.reverse(arr));
+                positionFlowable = Flowable.intervalRange(0, arr.size(), 0, 100, TimeUnit.MILLISECONDS)
+                        .repeat()
+                        .map(aLong -> new Telemetry.Position(
+                                12.904993 + (12.906055 - 12.904993) * arr.get(aLong.intValue()) / (arr.size()/2),
+                                80.157708 + (80.159610 - 80.157708) * arr.get(aLong.intValue()) / (arr.size()/2),
+                                80f,
+                                20f
+                        ));
+            } else {
+                positionFlowable = mDrone.getTelemetry().getPosition()
+                        .throttleLast(THROTTLE_TIME_MILLIS, TimeUnit.MILLISECONDS)
+                        .subscribeOn(Schedulers.io());
+            }
+
+            mPositionLiveData = LiveDataReactiveStreams.fromPublisher(positionFlowable);
+        }
+
+        return mPositionLiveData;
+    }
+
+
+    public void destroy() {
+        mCompositeDisposable.dispose();
+        mDrone.dispose();
+        mMavsdkServer.stop();
+
+        if (mSerialManager != null) {
+            mSerialManager.stop();
+        }
+
+        if (mTcpManager != null) {
+            mTcpManager.stop();
+        }
+    }
+}
